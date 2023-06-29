04df01880a3a4a5639c432ddd56b2c678416c099
pratikgdh <134198023+pratikgdh@users.noreply.github.com>
2023-06-29 20:02:59 +0530

    Update test.php

--- test.php
+++ test.php
@@ -5,14 +5,19 @@ Description: Plugin to convert Amazon affiliate links based on visitor's country
 Author: Your Name
 Version: 1.0
 */
-
+function replace_amazon_url_in_content($content) {
 $response = wp_remote_get( 'https://www.amazon.in/dp/B0BQHBWPYX/?tag=lightappssk-21' );
 
 
 if( is_wp_error( $response ) ) {
-    echo 'There was an error making the request.';
+//     echo 'There was an error making the request.';
 } else {
-    echo 'Response:<pre>';
+//     echo 'Response:<pre>';
     print_r( $response );
-    echo '</pre>';
+//     echo '</pre>';
 }
+	
+}
+
+
+add_filter('the_content', 'replace_amazon_url_in_content');
